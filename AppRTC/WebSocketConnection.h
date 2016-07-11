4fb44f33446c30e02cecc55c3d055910268f3ba8
Simon Crequer <sjc291@uclive.ac.nz>
2016-07-11 15:24:24 +1200

    Initial commit?

---
+++ AppRTC/WebSocketConnection.h
@@ -0,0 +1,13 @@
+//
+//  WebSocketConnection.h
+//  AppRTC
+//
+//  Created by Simon Crequer on 11/07/16.
+//  Copyright © 2016 ISBX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebSocketConnection : NSObject
+
+@end
---
+++ AppRTC/WebSocketConnection.m
@@ -0,0 +1,13 @@
+//
+//  WebSocketConnection.m
+//  AppRTC
+//
+//  Created by Simon Crequer on 11/07/16.
+//  Copyright © 2016 ISBX. All rights reserved.
+//
+
+#import "WebSocketConnection.h"
+
+@implementation WebSocketConnection
+
+@end
